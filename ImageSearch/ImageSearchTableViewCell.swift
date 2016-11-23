3cafcfd2479a76f1b4d11086b0ae1a27accdd056
Denys Holub <den3007holub@gmail.com>
2016-11-23 15:42:40 +0200

    Initial Commit

--- ImageSearch/ImageSearchTableViewCell.swift
+++ ImageSearch/ImageSearchTableViewCell.swift
@@ -26,5 +26,5 @@ class ImageSearchTableViewCell: UITableViewCell {
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
   }
-
+  
 }
